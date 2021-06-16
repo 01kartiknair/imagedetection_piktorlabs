1ff1546bdfaacb30442af7c4d197e6e86f542547
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-06-16 18:02:20 +0530

    prettier and formating

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
-import Navbar from './Navbar';
-import Upload from './Upload';
+import Navbar from "./Navbar";
+import Upload from "./Upload";
 // import About from './About';
 
 function App() {
   return (
-      <div>
-          <Navbar/>
-          <Upload/>
-          {/* <About/> */}
-      </div>
-       
+    <div>
+      <Navbar />
+      <Upload />
+      {/* <About/> */}
+    </div>
   );
 }
 export default App;
